bb6dd287636abe8d2c38df0d4e7e89c201eb9c04
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2023-01-04 22:37:37 +1300

    WIP

--- src/Form/ClientSidePasswordField.php
+++ src/Form/ClientSidePasswordField.php
@@ -14,7 +14,7 @@ class ClientSidePasswordField extends PasswordField
     /**
      * @var array
      */
-    protected $config = [];
+    // protected $config = [];
 
     public function __construct($name, $title = null, $value = null)
     {
@@ -25,69 +25,49 @@ class ClientSidePasswordField extends PasswordField
 
     public function Type()
     {
-        return 'client-side passsword';
+        return 'text password client-side-passsword';
     }
-
-    /**
-     * @param string $name
-     * @param mixed  $val
-     */
-    public function setConfig($name, $val = null)
-    {
-        if (is_array($name) && null === $val) {
-            foreach ($name as $n => $value) {
-                $this->setConfig($n, $value);
-            }
-
-            return $this;
-        }
-
-        if (is_array($this->config[$name])) {
-            if (! is_array($val)) {
-                user_error("The value for {$name} must be an array");
-            }
-
-            $this->config[$name] = array_merge($this->config[$name], $val);
-        } elseif (isset($this->config[$name])) {
-            $this->config[$name] = $val;
-        }
-
-        return $this;
-    }
-
-    /**
-     * @param string $name Optional, returns the whole configuration array if empty
-     *
-     * @return array|mixed
-     */
-    public function getConfig($name = null)
-    {
-        if ($name) {
-            return isset($this->config[$name]) ? $this->config[$name] : null;
-        }
-
-        return $this->config;
-    }
-
-    /**
-     * Set additional attributes.
-     *
-     * @return array Attributes
-     */
-    public function getAttributes()
-    {
-        $parentAttributes = parent::getAttributes();
-        $attributes = [];
-
-        if (! isset($parentAttributes['placeholder'])) {
-            $attributes['placeholder'] = $this->config['defaultparts']['scheme'] . '://example.com'; //example url
-        }
-
-        return array_merge(
-            $parentAttributes,
-            $attributes
-        );
-    }
+    //
+    // /**
+    //  * @param string $name
+    //  * @param mixed  $val
+    //  */
+    // public function setConfig($name, $val = null)
+    // {
+    //     if (is_array($name) && null === $val) {
+    //         foreach ($name as $n => $value) {
+    //             $this->setConfig($n, $value);
+    //         }
+    //
+    //         return $this;
+    //     }
+    //
+    //     if (is_array($this->config[$name])) {
+    //         if (! is_array($val)) {
+    //             user_error("The value for {$name} must be an array");
+    //         }
+    //
+    //         $this->config[$name] = array_merge($this->config[$name], $val);
+    //     } elseif (isset($this->config[$name])) {
+    //         $this->config[$name] = $val;
+    //     }
+    //
+    //     return $this;
+    // }
+    //
+    // /**
+    //  * @param string $name Optional, returns the whole configuration array if empty
+    //  *
+    //  * @return array|mixed
+    //  */
+    // public function getConfig($name = null)
+    // {
+    //     if ($name) {
+    //         return isset($this->config[$name]) ? $this->config[$name] : null;
+    //     }
+    //
+    //     return $this->config;
+    // }
 
     /**
      * Server side validation.
